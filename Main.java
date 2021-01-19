a85d699d385d71ac481510a0b7e54f764fbeac46
Mario Castelo <91093467@g.mvusd.net>
2021-01-19 19:59:40 +0000

    Added a label to the panel

--- Main.java
+++ Main.java
@@ -36,8 +36,12 @@ public class Main
       }
     });
 
+    // ------------------- Label 1 ---------------------
+    JLabel label1 = new JLabel("Count the ducks");
+
     panel1.add(button1);
     panel1.add(button2);
+    panel1.add(label1);
 
     frame1.getContentPane().add(panel1);
   } // end main method
